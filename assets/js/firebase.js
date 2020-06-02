df8e06bae5b8341214dd504c3702aa7a15670479
sharshi <sharshib@gmail.com>
2020-06-02 10:06:39 -0400

    form validation

---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDL44dxFVdpGapGq6L7ZP9IQ4EX-u_5jIg",
+  authDomain: "tidy-albatross.firebaseapp.com",
+  databaseURL: "https://tidy-albatross.firebaseio.com",
+  projectId: "tidy-albatross",
+  storageBucket: "tidy-albatross.appspot.com",
+  messagingSenderId: "423997099062",
+  appId: "1:423997099062:web:108f31d786e0ee73109c97",
+  measurementId: "G-GGLE1TM8CJ"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
