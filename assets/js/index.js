ee2425524c24cbb6b432271c7ca653abfc3337f5
Shersheial Borisute <4740525+sharshi@users.noreply.github.com>
2021-12-28 21:25:16 -0500

    basic site

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,55 +12,7 @@ window.addEventListener("DOMContentLoaded", () => {
           title: "app store",
         },
       ],
-    },
-    {
-      image: "images/wh-screenshot.png",
-      title: "Web Hunt",
-      description:
-        "Web Hunt, is a single page web application inspired by Product Hunt. It allow creators to share their products and receive feedback from developers and tech enthusiasts.",
-      links: [
-        {
-          url: "https://webhunt.dev",
-          title: "live",
-        },
-        {
-          url: "https://github.com/sharshi/Web-Hunt",
-          title: "github",
-        },
-      ],
-    },
-    {
-      image: "images/morning-dash-screenshot.png",
-      title: "Morning Dash",
-      description:
-        "Morning Dash is your day at a glance. See local weather, your commute, and calendar events all in one place.",
-      links: [
-        {
-          url: "https://themorningdash.herokuapp.com/",
-          title: "live",
-        },
-        {
-          url: "https://github.com/sharshi/Morning-Dash",
-          title: "github",
-        },
-      ],
-    },
-    {
-      image: "images/tidy-albatross-screenshot.png",
-      title: "Tidy Albatross",
-      description:
-        "An endless runner game where users help an Albatross clean a beach.",
-      links: [
-        {
-          url: "http://tidyalbatross.com",
-          title: "live",
-        },
-        {
-          url: "https://github.com/sharshi/TidyAlbatross",
-          title: "github",
-        },
-      ],
-    },
+    }
   ]);
 });
 
